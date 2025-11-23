211d2d08304951c6a2f49b4c98ec1757b02b57b2
egor24912 <dorogovegor0@gmail.com>
2025-11-23 11:52:14 +0200

    second commit

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
 let donuts = document.querySelectorAll('.donut')
+let dnts = document.querySelector('.donuts')
+let inp1 = document.querySelector('.inp1')
+let button1 = document.querySelector('.inp-commit1')
+let inp2 = document.querySelector('.inp2')
+let button2 = document.querySelector('.inp-commit2')
 
-donuts.forEach(donut => {
-    console.log(donut)
-    donut.style.height = '500px'
+
+button1.addEventListener('click', function() {
+    let val1 = inp1.value
+    donuts.forEach(donut => {
+        donut.style.height = `${val1}px`
+    })
 })
+
+button2.addEventListener('click', function() {
+    let val2 = inp2.value
+    dnts.innerHTML = ``
+    for (let i = 0; i < val2; i++) {
+        
+        dnts.innerHTML += `<img src='https://learn.algoritmika.org/uploads/2023/02/donut.png' class='donut'/>`
+    }
+})
+
+
